aba79bbfa3594fe4ffc0c7d1a3a874acb1dbd0d0
Vasil Vanchuck <v.vanchuk@tut.by>
2014-06-23 13:14:04 +0300

    Add checkform functions

--- inc/functions.php
+++ inc/functions.php
@@ -26,3 +26,108 @@ function processTemplace($tpl, array $data = array()){
     }
     return $tpl;
 }
+
+/**
+ * Проверяет была ли отправлена форма
+ * @return bool
+ */
+function isFormSubmitted(){
+    return (isset($_POST) AND !empty($_POST));
+}
+
+/**
+ * Возвращает массив данных ассоциированных с формой
+ * @return array
+ */
+function getFormData(){
+    $arr = array();
+    $arr['userName'] = isset($_POST['userName'])? trim($_POST['userName']): "";
+    $arr['userEmail'] = isset($_POST['userEmail'])? trim($_POST['userEmail']): "";
+    $arr['messageText'] = isset($_POST['messageText'])? trim($_POST['messageText']): "";
+    $arr['captcha'] = isset($_POST['captcha'])? trim($_POST['captcha']): "";
+    return $arr;
+}
+
+/**
+ * Проверяет правильность введенной капчи
+ * @return bool
+ */
+function checkCaptchaAnswer($answ){
+    $rightAnsw = isset($_SESSION['captcha'])? $_SESSION['captcha']: '';
+    return $answ == $rightAnsw;
+}
+
+/**
+ * Проверяеь валидность заполлнения полей формы
+ * @param array $formData - массив с данными формы
+ * @return array|bool - TRUE если форма валидка, массив со списком ошибок, если нет
+ */
+function isFormVaild(array $formData){
+    $resp = true;
+    $errors = array();
+
+    if(strlen($formData['userName']) < 5){
+        $resp = false;
+        $errors['userName'] = 'Проверьте ввод имени';
+    }
+
+    if(strlen($formData['userEmail']) < 5){
+        $resp = false;
+        $errors['userEmail'] = 'Проверьте ввод email';
+    }
+
+
+    if(strlen($formData['messageText']) < 50){
+        $resp = false;
+        $errors['messageText'] = 'Сообщение должно содержать от 50 символов';
+    }
+
+    if(!checkCaptchaAnswer($formData['captcha'])){
+        $resp = false;
+        $errors['captcha'] = 'Неправильный ответ';
+    }
+
+    if(!$resp){
+        return $errors;
+    } else {
+        return $resp;
+    }
+}
+
+/**
+ * Выводит сообщения об ошибках в переданный шаблон
+ * @param $tpl - входной html
+ * @param array $data
+ * @return string
+ */
+function processTemplateErrorOutput($tpl, array $data = array()){
+    foreach($data as $key => $val){
+        $tpl = str_replace(
+            "<p class=\"help-block\" data-name=\"$key\"></p>",
+            "<p class=\"help-block\" data-name=\"$key\">$val</p>",
+            $tpl
+        );
+    };
+
+    return $tpl;
+}
+
+function generateCaptcha(){
+    $answ = rand(1, 20);
+    $marker = rand(0,1)? '+': '-';
+
+    $b = rand(1,$answ);
+    $a;
+    switch($marker){
+        case '+':
+            $a = $answ - $b;
+            break;
+        case '-':
+            $a = $answ + $b;
+            break;
+    }
+
+    $_SESSION['captcha'] = $answ;
+    return $a.' '.$marker.' '.$b;
+
+}
--- index.php
+++ index.php
@@ -3,10 +3,24 @@
 require_once('inc/inc.php');
 
 $pageTpl = getTemplate('page');
-$formTpl = getTemplate('form');
+$form = getTemplate('form');
+
+$formData = getFormData();
+
+if(isFormSubmitted()){
+    $validateFormResult = isFormVaild($formData);
+    if($validateFormResult!== true) {
+        $form = processTemplateErrorOutput($form, $validateFormResult);
+    } else {
+        $formData['messageText'] = "";
+    }
+}
+
+$form = processTemplace($form, $formData);
 
 $page = processTemplace($pageTpl, array(
-    'FORM' => $formTpl
+    'FORM' => $form,
+    'CAPTCHA' => generateCaptcha()
 ));
 
 echo $page;
